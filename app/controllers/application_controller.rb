26904d92d4bb9f27bfa0601855940b5cbc18e27b
Collin <collinthedavis@gmail.com>
2017-02-01 16:42:15 -0700

    Using sessions to share data between pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+	include DeviseWhitelist
+	
+  before_action :set_source
 
-  include DeviseWhitelist
+  def set_source
+    session[:source] = params[:q] if params[:q]
+  end
 end
